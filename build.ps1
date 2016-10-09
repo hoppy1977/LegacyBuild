a36c5469a4f8e381bfc811df91c1f962a8e89eeb
Shane Hopcroft <shanehopcroft@hotmail.com>
2016-10-10 09:17:34 +1100

    Initial commit.

---
+++ build.ps1
@@ -0,0 +1,15 @@
+﻿$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+#$solution = "C:\work\svn\trunk\WINDEV4\AllApp\AllApp.sln"
+$solution = "C:\Users\Shane\Documents\hopatech\CodeAnalysisTest\CodeAnalysisTest.sln"
+$configuration = "Release"
+$parameters = "/p:Configuration=" + $configuration
+
+& $msbuild $parameters $solution "/m"
+if($LastExitCode -ne 0)
+{
+    echo "Failure"
+    exit
+}
+
+echo "Success"
